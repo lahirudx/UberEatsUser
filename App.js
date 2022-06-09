fe57055d43d69f1544e6d934084f778b398e82d5
Lahiru Amarasooriya <lahirudx@gmail.com>
2022-06-09 16:01:25 +0530

    Added basic restaurant component

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import restaurants from './assets/data/restaurants.json';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.restaurantContainer}>
+        <Image source={{ uri: restaurants[0].image }}
+          style={styles.image}
+        />
+        <Text style={styles.title}>
+          {restaurants[0].dishes[0].name}
+        </Text>
+        <Text style={styles.subtitle}>
+          {`$ ${restaurants[0].dishes[0].price}`}
+        </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +27,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10
   },
+  restaurantContainer: {
+    width: '100%'
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 5/3,
+    marginBottom: 5,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '500',
+    marginVertical: 5,
+  },
+  subtitle: {
+    color: 'grey'
+  }
 });
